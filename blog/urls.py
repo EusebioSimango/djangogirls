81ebfbcf87eb87d473bd97d2955ee52cdd52a5f8
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-12 17:20:30 +0200

    Improvising

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name="post_list")
+    path('', views.post_list, name="post_list"),
+    path('<str:author>/<str:title>', views.post_info, name="post_info")
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,9 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_info(request, title, author):
+    post = Post.objects.filter(title=title).first()
+    print(post.title)
+    return render(request, 'blog/post_info.html', {'post': post})
