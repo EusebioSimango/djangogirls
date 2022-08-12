6942b153402a0f4d93754ca010b138c3b579becc
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-12 18:48:07 +0200

    finished

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
-    path('<str:author>/<str:title>', views.post_info, name="post_info")
+    path('post/new', views.post_new, name="post_new"),
+    path('<str:author>/<str:title>', views.post_info, name="post_info"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.utils import timezone
+
+from blog.forms import PostForm
 from .models import Post
 # Create your views here.
 
@@ -11,5 +13,18 @@ def post_list(request):
 
 def post_info(request, title, author):
     post = Post.objects.filter(title=title).first()
-    print(post.title)
     return render(request, 'blog/post_info.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
